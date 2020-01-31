78c0d34d552ed8a1c2f194b3b654c59d9993eab6
Will Smith <wismith@davidson.edu>
2020-01-31 10:44:04 -0800

    added error messaging for CLA feature

--- textalyze.js
+++ textalyze.js
@@ -5,21 +5,41 @@
 // comments are here to help you, but please delete them as you go along.
 
 // Open lib/itemCounts.js to get started on the first iteration.
-let itemCounts = require('./lib/itemCounts');
+// let itemCounts = require('./lib/itemCounts');
+
 let frequencyStats = require('./lib/frequencyStats');
 let readFileSync = require('./lib/readFileSync');
 let sanitize = require('./lib/sanitize');
 let stringToChars = require('./lib/stringToChars');
 let process = require('process');
+let fs = require('fs');
 let helpers = require('./printHelpers');
-let file = process.argv[2];
 
-function textalyze(file) {
+function printUsage() {
+  console.log('Usage');
+  console.log(' node textalyze.js <fileName>');
+}
+function printError(errorMsg) {
+  console.log(`Error: ${errorMsg}`);
+}
+function printErrorAndExit(errorMsg) {
+  printError(errorMsg);
+  printUsage();
+  process.exit();
+}
+function textalyze() {
+  let file = process.argv[2];
+  // Implement error for incorrect usage
+  if (file === undefined) {
+    printErrorAndExit('Error: No file given to analyze.');
+  }
+  if (!fs.existsSync(file)) {
+    printErrorAndExit(`No such file ${file}`);
+  }
   return frequencyStats(stringToChars(sanitize(readFileSync(file))));
 }
 
-//console.log(textalyze('./sample_data/great-gatsby.txt'));
-
+// console.log(textalyze('./sample_data/great-gatsby.txt'));
 
 // let counts = itemCounts(sampleInput);
 
@@ -33,7 +53,7 @@ for (let item of Object.keys(counts)) {
 }
 */
 
-let data = textalyze(file); // "data" will be an object/dictionary
+let data = textalyze(); // "data" will be an object/dictionary
 
 for (let key of Object.keys(data)) {
   let percent = (100 * data[key]).toFixed(2);
